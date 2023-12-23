c61d9df280d25f4ef912b04662fe68f9ab10cac9
Joanalnu <139717293+joanalnu@users.noreply.github.com>
2023-12-23 15:49:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,42 @@
-// Fetch comments from GitHub Issues API
-fetch('https://api.github.com/repos/yourusername/yourrepository/issues/1/comments')
-  .then(response => response.json())
-  .then(comments => {
-    // Render comments on your webpage
-    const commentsList = document.getElementById('comments-list');
+const commentForm = document.getElementById('comment-form');
+const commentsList = document.getElementById('comments-list');
 
-    comments.forEach(comment => {
-      const commentDiv = document.createElement('div');
-      commentDiv.textContent = comment.body;
-      commentsList.appendChild(commentDiv);
-    });
+commentForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const commentInput = document.getElementById('comment-input');
+  const commentText = commentInput.value.trim();
+
+  if (commentText !== '') {
+    postComment(commentText);
+    commentInput.value = '';
+  }
+});
+
+function postComment(comment) {
+  const accessToken = 'YOUR_ACCESS_TOKEN'; // Replace with your GitHub access token
+  const issueNumber = 1; // Replace with the issue number where comments are stored
+
+  fetch(`https://api.github.com/repos/yourusername/yourrepository/issues/${issueNumber}/comments`, {
+    method: 'POST',
+    headers: {
+      Authorization: `token ${accessToken}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ body: comment }),
   })
-  .catch(error => {
-    console.error('Error fetching comments:', error);
-  });
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok.');
+      }
+      return response.json();
+    })
+    .then(data => {
+      // Optional: You can display the newly added comment instantly
+      const commentDiv = document.createElement('div');
+      commentDiv.textContent = data.body;
+      commentsList.appendChild(commentDiv);
+    })
+    .catch(error => {
+      console.error('Error adding comment:', error);
+    });
+}
