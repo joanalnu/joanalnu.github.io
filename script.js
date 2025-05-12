8a8968377cbe1ea06c5b62107665d343c182ea99
joanalnu <joanalnu@outlook.com>
2025-05-12 23:10:29 +0200

    convert replacement into main website

--- script.js
+++ script.js
@@ -1,33 +1,23 @@
+function toggleMobileNavState() {
+    const a=document.querySelector("body");
+    a.classList.toggle("nav--active")
+}
+function initBurger() {
+    const a=document.querySelector(".burger");
+    a.addEventListener("click",toggleMobileNavState)
+}
+initBurger();
+
 function scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 }
 
-// Modal features
-function openModal(modalId) {
-    document.getElementById(modalId).style.display = 'block';
-}
-function closeModal(modalId) {
-    document.getElementById(modalId).style.display = 'none';
-}
-window.onclick = function(event) {
-    const modals = document.getElementsByClassName('modal');
-    for (let modal of modals) {
-        if (event.target == modal) {
-            modal.style.display = 'none';
-        }
-    }
-}
-
-// toggle menu for hamburger icon
-function toggleMenu() {
-    const menu = document.querySelector('.menu');
-    menu.classList.toggle('active');
-}
-
-// Accessibility: toggle menu with keyboard (Enter key)
+/* Accessibility: toggle menu with keyboard (Enter key)
 document.querySelector('.hamburger-icon').addEventListener('keydown', function(e) {
     if (e.key === 'Enter' || e.key === ' ') {
         e.preventDefault();
         toggleMenu();
     }
 });
+*/
+
