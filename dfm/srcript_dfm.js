8f936d1a5bc91f1587c531b44f32ce26a6d45858
joanalnu <joanalnu@outlook.com>
2025-05-12 15:55:13 +0200

    start design update in sub-dir

---
+++ dfm/srcript_dfm.js
@@ -0,0 +1,13 @@
+function toggleMobileNavState() {
+    const a=document.querySelector("body");
+    a.classList.toggle("nav--active")
+}
+function initBurger() {
+    const a=document.querySelector(".burger");
+    a.addEventListener("click",toggleMobileNavState)
+}
+initBurger();
+
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
